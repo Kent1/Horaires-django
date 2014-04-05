71574729e7006b438e8017fbcea22581bad4722a
Jérôme Dauge <jerome.dauge@gmail.com>
2014-04-05 09:20:58 +0200

    Move convert_timeslot_todate() and assign_color() to util.py

--- examination_timetable/timetable/util.py
+++ examination_timetable/timetable/util.py
@@ -1,3 +1,5 @@
+import datetime
+
 def day_per_we():
     return 1
 
@@ -17,3 +19,51 @@ def last_timetable_scheduled(timetable):
         return None, None
     else:
         return timetable[0], timetable[0].exams.all()
+
+def convert_timeslot_to_date(exam, timetable):
+    real_timeslot = exam.timeslot - timetable.start.weekday()*2
+
+    exam.date = timetable.start + datetime.timedelta(
+        days=(get_day_delta(timetable.start, real_timeslot)))
+    time = exam.date.timetuple()
+    exam.month = time.tm_mon - 1
+    exam.day = time.tm_mday
+    exam.m_start = 15
+    exam.m_end = 15
+    if exam.timeslot % 2 == 0:
+        exam.h_start = 8
+        exam.h_end = 12
+        exam.color = 'blue'
+    else:
+        exam.h_start = 13
+        exam.h_end = 17
+        exam.color = 'green'
+
+def assign_color(exam, colors, last_color):
+    assignation = False
+    list_exam_studs = exam.students.all()
+
+    for students, color in colors:
+        counter = 0.0
+        list_studs = students.all()
+
+        for student in list_exam_studs:
+            if student in list_studs:
+                counter += 1
+
+        if counter/len(list_exam_studs) > 0.8:
+            exam.color = color
+            assignation = True
+
+    if not assignation:
+        if last_color[-1] == 0:
+            last_color[0] += (255 - last_color[0])/3
+        elif last_color[-1] == 1:
+            last_color[1] = last_color[0]*2
+        else:
+            last_color[2] = last_color[1]
+        last_color[-1] = (last_color[-1]+1)%3
+
+        color = 'rgb(%d,%d,%d)' % tuple(last_color[:3])
+        colors.append((exam.students, color))
+        exam.color = color
--- examination_timetable/timetable/views.py
+++ examination_timetable/timetable/views.py
@@ -1,4 +1,3 @@
-import datetime
 from django.template import Context, loader
 from django.shortcuts import render, render_to_response
 from timetable.models import Student, Professor, Exam, Timetable, Room
@@ -15,7 +14,7 @@ def student(request, student_id):
         students = exam.students.all()
         for stud in students:
             if stud.pk == student.pk:
-                convert_timeslot_to_date(exam, timetable)
+                util.convert_timeslot_to_date(exam, timetable)
                 my_exam.append(exam)
 
     return render_to_response('timetable.html',
@@ -52,7 +51,7 @@ def professor(request, professor_id):
     my_exam = list()
     for exam in exams:
         if exam.professor.pk == professor.pk:
-            convert_timeslot_to_date(exam, timetable)
+            util.convert_timeslot_to_date(exam, timetable)
             my_exam.append(exam)
     return render_to_response('timetable.html',
                                 {
@@ -66,7 +65,7 @@ def exam(request, exam_id):
     timetable, exams = util.last_timetable_scheduled(Timetable.objects.all())
     if timetable == None:
         return render_to_response('error.html')
-    convert_timeslot_to_date(exam, timetable)
+    util.convert_timeslot_to_date(exam, timetable)
     return render_to_response('exam.html',
                                 {
                                     'exam' : exam,
@@ -77,8 +76,7 @@ def exams(request):
     if timetable == None:
         return render_to_response('error.html')
     for exam in exams:
-        print exam.timeslot
-        convert_timeslot_to_date(exam, timetable)
+        util.convert_timeslot_to_date(exam, timetable)
     return render_to_response('exams.html',
                                 {
                                     'exams' : exams,
@@ -93,8 +91,8 @@ def all_exams(request):
     colors = []
     last_color = [0, 0, 0, 0]
     for exam in exams:
-        convert_timeslot_to_date(exam, timetable)
-        assign_color(exam, colors, last_color)
+        util.convert_timeslot_to_date(exam, timetable)
+        util.assign_color(exam, colors, last_color)
 
     return render_to_response('timetable.html',
                                 {
@@ -112,7 +110,7 @@ def room(request, room_id):
     exams = Exam.objects.all()
     for exam in exams:
         if exam.room.pk == room.pk:
-            convert_timeslot_to_date(exam, timetable)
+            util.convert_timeslot_to_date(exam, timetable)
             my_exam.append(exam)
     return render_to_response('timetable.html',
                                 {
@@ -144,52 +142,3 @@ def index(request):
                                     'professors' : professors,
                                     'exams' : exams
                                 })
-
-
-def convert_timeslot_to_date(exam, timetable):
-    real_timeslot = exam.timeslot - timetable.start.weekday()*2
-
-    exam.date = timetable.start + datetime.timedelta(
-        days=(util.get_day_delta(timetable.start, real_timeslot)))
-    time = exam.date.timetuple()
-    exam.month = time.tm_mon - 1
-    exam.day = time.tm_mday
-    exam.m_start = 15
-    exam.m_end = 15
-    if exam.timeslot % 2 == 0:
-        exam.h_start = 8
-        exam.h_end = 12
-        exam.color = 'blue'
-    else:
-        exam.h_start = 13
-        exam.h_end = 17
-        exam.color = 'green'
-
-def assign_color(exam, colors, last_color):
-    assignation = False
-    list_exam_studs = exam.students.all()
-
-    for students, color in colors:
-        counter = 0.0
-        list_studs = students.all()
-
-        for student in list_exam_studs:
-            if student in list_studs:
-                counter += 1
-
-        if counter/len(list_exam_studs) > 0.8:
-            exam.color = color
-            assignation = True
-
-    if not assignation:
-        if last_color[-1] == 0:
-            last_color[0] += (255 - last_color[0])/3
-        elif last_color[-1] == 1:
-            last_color[1] = last_color[0]*2
-        else:
-            last_color[2] = last_color[1]
-        last_color[-1] = (last_color[-1]+1)%3
-
-        color = 'rgb(%d,%d,%d)' % tuple(last_color[:3])
-        colors.append((exam.students, color))
-        exam.color = color
